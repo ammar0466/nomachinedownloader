08d6c0323a905a4b4fb86a5e779a0da406b3b72d
ammar <ammar0466@gmail.com>
2022-11-25 12:36:18 +0800

    edit

--- nomachineScraper.py
+++ nomachineScraper.py
@@ -20,7 +20,7 @@ print('Downloading '+href.split('/')[-1])
 requests.get(href, stream=True)
 response = requests.get(href, stream=True)
 total_size_in_bytes= int(response.headers.get('content-length', 0))
-block_size = 1024 #1 Kibibyte
+block_size = 1024 #1 Kilobyte
 progress_bar = tqdm.tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
 
 
